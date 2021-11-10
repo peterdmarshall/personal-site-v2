c3cdec64c7b7cda24f7d6f151f6542e99e85b908
Peter Marshall <peterdmarshall99@gmail.com>
2021-11-10 16:19:07 -0500

    Update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,20 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingId: "G-K3HPT5C2ZY",
+        trackingIds: ["G-K3HPT5C2ZY"],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
       },
     },
     'gatsby-plugin-postcss'
