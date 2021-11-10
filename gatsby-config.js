7e6b4870f02d7027b3f958ead0b42a4ef0ee68f5
Peter Marshall <peterdmarshall99@gmail.com>
2021-11-10 12:57:00 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Personal Site V2",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-K3HPT5C2ZY",
+      },
+    },
+  ],
+};
